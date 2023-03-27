22781969156e2ef9898b699baeb5d89f5e2fb09a
zhao-yiming <zhao.yiming423@gmail.com>
2023-03-27 20:29:21 +0200

    CRUD for extra

--- api/controllers/extraController.js
+++ api/controllers/extraController.js
@@ -13,3 +13,34 @@ exports.extraDetails = function (req,res){
     
     
 }
+
+exports.extraList = function (req,res){
+    res.json({"extras":extras});
+}
+
+
+exports.deleteExtra = function (req,res){
+    let id = req.params.extra_id;
+    
+    if (id >=0 && id < extras.length){
+        let extraToRemove = extras[id];
+        extras.splice(id,1);
+        res.json({id:extraToRemove});
+    }
+    else{
+        res.status(404).json({"message":"id out or range"});
+    }
+}
+
+exports.createExtra = function (req,res){
+    let name = req.body.name;
+    let price = req.body.price;
+    if (name==undefined || price == undefined){
+        res.status(404).json({"error":"invalid body format", "valid_keys":["name","price"]});
+    }
+    else{
+        let extra = new Extra(name,price);
+        extras.push(extra);
+        res.json({id:extra});
+    }
+}
