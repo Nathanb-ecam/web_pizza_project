f799629dd3ca94dac753f27c562a9728e00b088e
zhao-yiming <zhao.yiming423@gmail.com>
2023-03-27 20:34:45 +0200

    add CRUD route

--- api/routes.js
+++ api/routes.js
@@ -12,9 +12,24 @@ router.get('/',(req,res)=>{
     res.json({"message":"Welcome to pizza hut"});
 });
 
-router.get('/pizza/:pizza_id',pizzaController.pizzaDetails);
+//chicken
 router.get('/chicken/:chicken_id',chickenController.chickenDetails);
+router.get('/chickens/',chickenController.chickenList);
+router.delete('/chicken/:chicken_id',chickenController.deleteChicken);
+router.post('/chicken',chickenController.createChicken);
+
+
+//sauce
 router.get('/sauce/:sauce_id',sauceController.sauceDetails);
+router.get('/sauces/',sauceController.sauceList);
+router.delete('/sauce/:sauce_id',sauceController.deleteSauce);
+router.post('/sauce',sauceController.createSauce);
+
+
+//extra
 router.get('/extra/:extra_id',extraController.extraDetails);
+router.get('/extras/',extraController.extraList);
+router.delete('/extra/:extra_id',extraController.deleteExtra);
+router.post('/extra',extraController.createExtra);
 
 module.exports = router;
