55722011b173208de4790e8e7d580fa24cb8aa10
zhao-yiming <zhao.yiming423@gmail.com>
2023-03-29 11:26:55 +0200

    chicken controller ORM structure

--- api/controllers/chickenController.js
+++ api/controllers/chickenController.js
@@ -1,46 +1,38 @@
 let Chicken = require('../models/chickenModel');
 
-let chickens = [new Chicken("Wings",12),new Chicken("Legs",10),new Chicken("Mixtes",11)]
 
-exports.chickenDetails = function (req,res){
-    let chicken_id  =req.params.chicken_id;
-    if (chicken_id >=0 && chicken_id < chickens.length){
-        res.json({chicken_id:chickens[chicken_id]});
-    }
-    else{
-        res.status(404).json({"message":"id out or range"});
-    }
-    
-    
+exports.listChicken = function (req,res){
+    Chicken.findAll({ attributes: ['chicken_id','chicken_name', 'price']} )
+        .then(data => {
+            // console.log(data.toJSON());
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message })
+    })
 }
 
-exports.chickenList = function (req,res){
-    res.json({"chickens":chickens});
+exports.searchChicken = function(req,res){
+    Chicken.findOne({ where: { chicken_id: req.params.chicken_id } })
+        .then(data=>res.json(data))
+        .catch(err=>res.status(500).json({message:err.message})) 
+}
+
+exports.createChicken = async function(req,res){
+    let chicken = Chicken.build({ chicken_name: req.body.chicken_name, price: req.body.price  })
+    // save object in DB
+    await chicken.save()
+        .then(data => {
+            res.json(data);
+    })
+        .catch(err => {
+            res.status(500).json({ message: err.message })
+    })
 }
 
 
 exports.deleteChicken = function (req,res){
-    let id = req.params.chicken_id;
-    
-    if (id >=0 && id < chickens.length){
-        let chickenToRemove = chickens[id];
-        chickens.splice(id,1);
-        res.json({id:chickenToRemove});
-    }
-    else{
-        res.status(404).json({"message":"id out or range"});
-    }
-}
-
-exports.createChicken = function (req,res){
-    let name = req.body.name;
-    let price = req.body.price;
-    if (name==undefined || price == undefined){
-        res.status(404).json({"error":"invalid body format", "valid_keys":["name","price"]});
-    }
-    else{
-        let chicken = new Chicken(name,price);
-        chickens.push(chicken);
-        res.json({id:chicken});
-    }
+    Chicken.destroy({ where: { chicken_id: req.params.chicken_id } })
+        .then(data=>res.json(data))
+        .catch(err=>res.status(500).json({message:err.message}))
 }
