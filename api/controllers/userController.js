3e788c79fb2a76cc01fcbb2ab66dfb09bff9d23a
Nathan BUCHIN <18092@ecam.be>
2023-06-09 21:35:13 +0200

    hashed passwords

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,12 +1,24 @@
+const bcrypt = require('bcryptjs');
+
 let User = require('../models/userModel');
 
+
+
 exports.searchUserByCredentials = async function(req,res){
-    await User.findOne({where:{name:req.body.name,password:req.body.password}} )
-    .then(data =>
-        req.data= data)
+    const {name,password} = req.body;
+
+    await User.findOne({where:{name:name}} )
+    .then(async (user) =>{
+        if(user){
+            const isPasswordMatch = await bcrypt.compare(password,user.password);
+            if(isPasswordMatch){
+                req.data = user;
+            }
+        }
+    })
     .catch(err => {
         res.status(500).json({ message: err.message })
-})
+    })
     }
     
 
@@ -34,15 +46,27 @@ exports.searchUser = function(req,res){
 }
 
 exports.createUser = async function(req,res){
-    let user = User.build({ name: req.body.name, password: req.body.password,isAdmin:req.body.isAdmin,points:req.body.points })
-    // save object in DB
-    await user.save()
-        .then(data => {
-            res.json(data);
-    })
-        .catch(err => {
-            res.status(500).json({ message: err.message })
-    })
+    const {name,password,isAdmin,points} = req.body;
+    const saltRounds = 10; // Number of salt rounds to use for hashing
+    try {
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      let user = User.build({ name: name, password: hashedPassword,isAdmin:isAdmin,points:points })
+      // save object in DB
+      await user.save()
+          .then(data => {
+              res.json(data);
+      })
+          .catch(err => {
+              res.status(500).json({ message: err.message })
+      })
+    } catch (err) {
+
+    }
+
+    
+    
+
+
 }
 
 
