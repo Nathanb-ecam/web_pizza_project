01b543710980ba50c2629c297ab58e31281951c2
Nathan BUCHIN <18092@ecam.be>
2023-06-09 17:47:38 +0200

    chicken error handling

--- api/controllers/chickenController.js
+++ api/controllers/chickenController.js
@@ -13,31 +13,58 @@ exports.listChicken = function (req,res){
 }
 
 exports.searchChicken = function(req,res){
-    Chicken.findOne({ where: { id: req.params.id } })
-        .then(data=>res.json(data))
-        .catch(err=>res.status(500).json({message:err.message})) 
+    const id = req.params.id;
+    if(!isNaN(id)){
+        Chicken.findOne({ where: { id: id } })
+            .then(data=>res.json(data))
+            .catch(err=>res.status(500).json({message:err.message})) 
+    }else{
+        res.status(400).json( {message:"Parameter 'id' must be a number"})
+    }
+
 }
+
 exports.searchChickenByName = function(req,res){
-    Chicken.findOne({ where: { name: req.params.name } })
+    const name =req.params.name;
+    if(isNaN(name)){
+        Chicken.findOne({ where: { name:name } })
         .then(data=>res.json(data))
         .catch(err=>res.status(500).json({message:err.message})) 
+    }
+    else{
+        res.status(400).json( {message:"Parameter 'name' must be a string"})
+
+    }
 }
 
 exports.createChicken = async function(req,res){
-    let chicken = Chicken.build({ name: req.body.name, price: req.body.price,desc:req.body.desc  })
-    // save object in DB
-    await chicken.save()
-        .then(data => {
-            res.json(data);
-    })
-        .catch(err => {
-            res.status(500).json({ message: err.message })
-    })
+    const {name,price, desc} = req.body;
+    // console.log(name,price,desc)
+    if(name=== null || name === undefined || price=== null || price ===undefined){
+        res.status(400).json({message:"Please provide all fields required : name and price"})
+    }else{
+        let chicken = Chicken.build({ name: name, price: price,desc:desc  })
+        // save object in DB
+        await chicken.save()
+            .then(data => {
+                res.json(data);
+        })
+            .catch(err => {
+                res.status(500).json({ message: err.message })
+        })
+    }
+    
 }
 
 
 exports.deleteChicken = function (req,res){
-    Chicken.destroy({ where: { id: req.params.id } })
+    const id = req.params.id;
+    if(!isNaN(id)){
+        Chicken.destroy({ where: { id: id } })
         .then(data=>res.json(data))
         .catch(err=>res.status(500).json({message:err.message}))
+    }else{
+        res.status(400).json({message:"Parameter 'id' must be a number"})
+    }
+    
 }
